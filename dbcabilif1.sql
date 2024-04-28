35527ea7aaeb9950c162bd7e503063cc478f8c89
Keiru <146981406+kwiruu@users.noreply.github.com>
2024-04-28 23:09:51 +0800

    Add files via upload

--- dbcabilif1.sql
+++ dbcabilif1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2024 at 04:27 PM
+-- Generation Time: Apr 28, 2024 at 05:09 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -51,6 +51,85 @@ INSERT INTO `tblactivityrecord` (`activityID`, `activityName`, `activityDescript
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tblcourse`
+--
+
+CREATE TABLE `tblcourse` (
+  `courseID` bigint(20) NOT NULL,
+  `courseName` varchar(100) NOT NULL COMMENT 'For Course Name',
+  `courseDescription` varchar(250) NOT NULL COMMENT 'This is for Course Description'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblcourse`
+--
+
+INSERT INTO `tblcourse` (`courseID`, `courseName`, `courseDescription`) VALUES
+(1, 'CSIT201', 'OPP'),
+(2, 'course202', 'awef'),
+(7, 'course202', 'awef'),
+(8, 'course202', 'awef'),
+(25, 'olok', 'otin'),
+(26, 'olok', 'otin'),
+(27, 'olok', 'otin'),
+(28, 'olok', 'otin'),
+(29, 'olok', 'otin'),
+(30, 'olok', 'otin'),
+(31, 'olok', 'otin'),
+(32, 'olok', 'otin'),
+(33, 'olok', 'otin'),
+(34, 'olok', 'otin'),
+(35, 'olok', 'otin'),
+(36, 'olok', 'otin'),
+(37, 'olok', 'otin'),
+(38, 'olok', 'otin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcoursestudent`
+--
+
+CREATE TABLE `tblcoursestudent` (
+  `courseStudentID` bigint(20) NOT NULL,
+  `courseID` bigint(20) NOT NULL,
+  `studentID` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcourseteacher`
+--
+
+CREATE TABLE `tblcourseteacher` (
+  `courseTeacherID` bigint(20) NOT NULL,
+  `teacherID` bigint(20) NOT NULL,
+  `courseID` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblcourseteacher`
+--
+
+INSERT INTO `tblcourseteacher` (`courseTeacherID`, `teacherID`, `courseID`) VALUES
+(1, 4, 25),
+(2, 4, 25),
+(3, 4, 25),
+(4, 4, 25),
+(5, 4, 25),
+(6, 4, 25),
+(7, 4, 25),
+(8, 4, 25),
+(9, 4, 25),
+(10, 4, 25),
+(11, 4, 25),
+(12, 4, 25),
+(13, 4, 25);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tblstudentrecord`
 --
 
@@ -67,7 +146,8 @@ CREATE TABLE `tblstudentrecord` (
 
 INSERT INTO `tblstudentrecord` (`studentID`, `acctid_fk_studentrecord`, `course`, `gradelvl`) VALUES
 (4, 9, 'BSIT', 2),
-(5, 10, 'BSIT', 2);
+(5, 10, 'BSIT', 2),
+(6, 12, 'BSIT', 2);
 
 -- --------------------------------------------------------
 
@@ -86,7 +166,8 @@ CREATE TABLE `tblteacherrecord` (
 --
 
 INSERT INTO `tblteacherrecord` (`teacherID`, `acctid_fk_teacherrecord`, `course`) VALUES
-(3, 11, 'BSIT');
+(3, 11, 'BSIT'),
+(4, 13, 'BSIT');
 
 -- --------------------------------------------------------
 
@@ -109,7 +190,9 @@ CREATE TABLE `tbluseraccount` (
 INSERT INTO `tbluseraccount` (`acctid`, `emailadd`, `username`, `password`, `usertype`) VALUES
 (9, 'keiru277@gmail.com', 'keiru277', '01230321', 'student'),
 (10, 'kurtcabili@gmail.com', 'kurt277', '01230321', 'student'),
-(11, 'keiru277@gmail.com', 'keiru277377', '01230321', 'teacher');
+(11, 'keiru277@gmail.com', 'keiru277377', '01230321', 'teacher'),
+(12, 'koy2', 'koyaskie', '123456', 'student'),
+(13, 'teacher', 'teacher', '123', 'teacher');
 
 -- --------------------------------------------------------
 
@@ -133,7 +216,9 @@ CREATE TABLE `tbluserprofile` (
 INSERT INTO `tbluserprofile` (`userid`, `acctid_fk_userprofile`, `firstname`, `lastname`, `gender`, `birthdate`) VALUES
 (27, 9, 'Keiru Vent', 'Cabili', 'male', '2004-06-03'),
 (28, 10, 'Kurt', 'Cabili', 'male', '2024-03-31'),
-(29, 11, 'Keiru Vent', 'Cabili', 'female', '2024-04-22');
+(29, 11, 'Keiru Vent', 'Cabili', 'female', '2024-04-22'),
+(30, 12, 'koyskie', 'nigger', 'male', '2024-04-19'),
+(31, 13, 'teacher', 'teacher', 'female', '0000-00-00');
 
 --
 -- Indexes for dumped tables
@@ -144,6 +229,27 @@ INSERT INTO `tbluserprofile` (`userid`, `acctid_fk_userprofile`, `firstname`, `l
 --
 ALTER TABLE `tblactivityrecord`
   ADD PRIMARY KEY (`activityID`);
+
+--
+-- Indexes for table `tblcourse`
+--
+ALTER TABLE `tblcourse`
+  ADD PRIMARY KEY (`courseID`);
+
+--
+-- Indexes for table `tblcoursestudent`
+--
+ALTER TABLE `tblcoursestudent`
+  ADD PRIMARY KEY (`courseStudentID`),
+  ADD KEY `courseID` (`courseID`),
+  ADD KEY `studentID` (`studentID`);
+
+--
+-- Indexes for table `tblcourseteacher`
+--
+ALTER TABLE `tblcourseteacher`
+  ADD PRIMARY KEY (`courseTeacherID`),
+  ADD KEY `teacherID` (`teacherID`);
 
 --
 -- Indexes for table `tblstudentrecord`
@@ -185,32 +291,63 @@ ALTER TABLE `tblactivityrecord`
   MODIFY `activityID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT for table `tblcourse`
+--
+ALTER TABLE `tblcourse`
+  MODIFY `courseID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `tblcoursestudent`
+--
+ALTER TABLE `tblcoursestudent`
+  MODIFY `courseStudentID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tblcourseteacher`
+--
+ALTER TABLE `tblcourseteacher`
+  MODIFY `courseTeacherID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `tblstudentrecord`
 --
 ALTER TABLE `tblstudentrecord`
-  MODIFY `studentID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `studentID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tblteacherrecord`
 --
 ALTER TABLE `tblteacherrecord`
-  MODIFY `teacherID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `teacherID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbluseraccount`
 --
 ALTER TABLE `tbluseraccount`
-  MODIFY `acctid` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'primary key, foreignkey to tbluserprofile', AUTO_INCREMENT=12;
+  MODIFY `acctid` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'primary key, foreignkey to tbluserprofile', AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbluserprofile`
 --
 ALTER TABLE `tbluserprofile`
-  MODIFY `userid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'UserID of the User', AUTO_INCREMENT=30;
+  MODIFY `userid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'UserID of the User', AUTO_INCREMENT=32;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `tblcoursestudent`
+--
+ALTER TABLE `tblcoursestudent`
+  ADD CONSTRAINT `courseID` FOREIGN KEY (`courseID`) REFERENCES `tblcourse` (`courseID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `studentID` FOREIGN KEY (`studentID`) REFERENCES `tblstudentrecord` (`studentID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tblcourseteacher`
+--
+ALTER TABLE `tblcourseteacher`
+  ADD CONSTRAINT `teacherID` FOREIGN KEY (`teacherID`) REFERENCES `tblteacherrecord` (`teacherID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tblstudentrecord`
