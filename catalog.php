ad93cb71c2631f56e3a1c8c5b2d2c25681628e61
kwiruu <keiru277@gmail.com>
2024-05-04 19:50:18 +0800

    updated profile-page, added user-profile images folder

--- catalog.php
+++ catalog.php
@@ -68,7 +68,118 @@
           </div>
       
     </div>
+    <div id="specialCoursesSection">
+                        <h4 style="color:black;">Special Courses You can Enroll</h4>
+                        <hr style="border-width: 1px; border-color: black;">
+                        <div class="course-container-main-box-act">
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Web Development</div>
+                                        <h5 class="open-sans-regular">Learn HTML</h5>
+                                        <span class="open-sans-regular">Learn HTML to kickstart your front-end career and embark on an exciting journey into web development. Mastering HTML is the first step towards crafting captivating websites and user interfaces that captivate audiences worldwide. </span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Machine Learning</div>
+                                        <h5 class="open-sans-regular">Learn Python</h5>
+                                        <span class="open-sans-regular">Learn Python to dive into the world of programming and open up a realm of possibilities in software development. Mastering Python is like unlocking a toolkit that allows you to build powerful applications, automate tasks, analyze data, and much more.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Web Development</div>
+                                        <h5 class="open-sans-regular">Learn JavaScript</h5>
+                                        <span class="open-sans-regular">Explore JavaScript to delve into the realm of programming and unleash a world of possibilities in software development. Mastering JavaScript is akin to acquiring a versatile toolset that empowers you to create dynamic web applications, handle user interactions, manipulate data, and more.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Data Analysis</div>
+                                        <h5 class="open-sans-regular">Learn MySQL</h5>
+                                        <span class="open-sans-regular">MySQL is an open-source relational database management system. Its name is a combination of "My", the name of co-founder Michael Widenius's daughter My, and "SQL", the acronym for Structured Query Language.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Machine Learning Basics</div>
+                                        <h5 class="open-sans-regular">Introduction to Machine Learning</h5>
+                                        <span class="open-sans-regular">Explore the fundamentals of machine learning, including supervised and unsupervised learning, classification, and regression.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Web Development</div>
+                                        <h5 class="open-sans-regular">Frontend Development with HTML, CSS, and JavaScript</h5>
+                                        <span class="open-sans-regular">Learn the building blocks of web development, including HTML for structure, CSS for styling, and JavaScript for interactivity.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Cybersecurity</div>
+                                        <h5 class="open-sans-regular">Ethical Hacking and Penetration Testing</h5>
+                                        <span class="open-sans-regular">Discover the principles and techniques of ethical hacking and penetration testing to strengthen cybersecurity defenses.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Python Programming</div>
+                                        <h5 class="open-sans-regular">Master Python Basics</h5>
+                                        <span class="open-sans-regular">Get started with Python programming language, covering basic syntax, data types, and control structures.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Artificial Intelligence</div>
+                                        <h5 class="open-sans-regular">Introduction to AI and Neural Networks</h5>
+                                        <span class="open-sans-regular">Learn the fundamentals of artificial intelligence and neural networks, including perceptrons, activation functions, and backpropagation.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Mobile App Development</div>
+                                        <h5 class="open-sans-regular">Building iOS Apps with Swift</h5>
+                                        <span class="open-sans-regular">Discover the essentials of iOS app development using Swift programming language, including UIKit, AutoLayout, and CoreData.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Data Science</div>
+                                        <h5 class="open-sans-regular">Data Visualization with Python</h5>
+                                        <span class="open-sans-regular">Learn how to create effective visualizations of data using Python libraries such as Matplotlib and Seaborn.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+
+                                <div class="course-container-act">
+                                    <div class="course-box-2">
+                                        <div class="course-box-top-box-2 space-mono-thin">Blockchain Technology</div>
+                                        <h5 class="open-sans-regular">Understanding Cryptocurrencies and Blockchain</h5>
+                                        <span class="open-sans-regular">Explore the principles behind cryptocurrencies and blockchain technology, including decentralized ledgers and smart contracts.</span>
+                                        <div class="course-container-footer open-sans-bold"><i class="fa-solid fa-chart-simple"></i> <u>view course</u></div>
+                                    </div>
+                                </div>
+
+
+                        </div>
+                    </div>
       </div>
+      
 
     
 </body>
--- profile.php
+++ profile.php
@@ -11,8 +11,8 @@ require_once 'includes/header.php';
     $query = "SELECT * FROM tbluserprofile WHERE userid = $userID";
     $query1 = "SELECT * FROM tbluseraccount WHERE acctid= $acctID";
 
-    $result = mysqli_query($con, $query);
-    $result1 = mysqli_query($con, $query1);
+    $result = mysqli_query($connection, $query);
+    $result1 = mysqli_query($connection, $query1);
 
     if ($result && mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
@@ -32,62 +32,170 @@ require_once 'includes/header.php';
 ?>
 
 <body>
-        
+        <br>
+        <br>
     <div class="profile-page-first-page">
-        <div>
-            <div style="display:flex; flex-direction: row; width:100%; padding-bottom:0px; margin-bottom:0px; border-bottom:1px solid black;">
-                <button class="toggle-button tab-button" style="width:7%;">Profile</button>
-                <button class="toggle-button tab-button" style="width:10%; margin-left:1%;">Edit profile</button>
-            </div>
+        <div class="yellow-top" style="height:50px">
         </div>
-        <div style="width: 100%;
-    padding-top: 5%;
-    margin: auto;
-    display: flex;">
-        <div class="profile-main-box">
-            
-            <div class="page">
-                <h1 class="space-mono-thin" style="color: black"><?php echo "$usertyper"; ?></h1>
-                <h3 class="open-sans-bold" style="color: black">@<?php echo "$username"; ?></h3>
-                    <div>
-                        <p><?php echo "$firstName";?></p>
-                        <p><?php echo "$lastName";?></p>
-                        <p><?php echo "$gender"; ?></p>
-                        <p><?php echo "$bdate"; ?></p>
-                        <p><?php echo "$email"; ?></p>
-                    </div>
-            </div>
-            <div class="page" style="display:none;">
-                    <h1 class="space-mono-thin" style="color: black"><?php echo "$usertyper"; ?></h1>
-                    <h3 class="open-sans-bold" style="color: black">@<?php echo "$username"; ?></h3>
-                    <div>
-                        <form action="profile.php" method="POST">
-                            <p>First Name: <input type="text" name="firstName" value="<?php echo $firstName; ?>"></p>
-                            <p>Last Name: <input type="text" name="lastName" value="<?php echo $lastName; ?>"></p>
-                            <p>Gender: <input type="text" name="gender" value="<?php echo $gender; ?>"></p>
-                            <p>Birthdate: <input type="date" name="bdate" value="<?php echo $bdate; ?>"></p>
-                            <p>Email: <?php echo "$email"; ?></p>
-                            <input type="submit" value="Save Changes">
-                        </form>
-                    </div>
+        <div class="profile-page-first-page-container">
+            <p class="open-sans-bold" style="margin-bottom:2%; font-size:20px">@<?php echo "$username"; ?>'s Profile</p>
+            <div style="display:flex; flex-direction:row; justify-content: space-between;">
+                <div class="profile-page-buttons">
+                    <button class="toggle-button" style="margin-bottom:0px; background-color:white">Profile</button>
+                    <button class="toggle-button" style="margin-bottom:0px; background-color:white">Edit profile</button>
+                    <button class="toggle-button" style="margin-bottom:0px; background-color:white">Settings</button>
                 </div>
-            </div>
-         
-        <div class="profile-right-box">
-            <h5 class="open-sans-bold">Current Assignments</h5>
-            <div class="assignment-box">
 
-            </div>
-        </div> 
-        
-        </div> 
-        
-        
-    </div> 
-    <div style="margin-top:5%;">
-        <div style="height:500px;">
+                <div class="profile-main-box page">
+                    <div class="name-box">
+                        <h5 class="open-sans-bold"><?php echo "$firstName";?> <?php echo "$lastName";?></h5>
+                    </div>
+                    <div class="profile-pic-box">
+                        <h6 style="margin-bottom:0px;">Profile image</h6>
+                        <div style="display:flex;flex-direction:row;justify-content: space-around;margin-left:15%">
+                            <img src="user-profiles/1.png" alt="profile-pic">
+                            <div style="display:flex;flex-direction:row;justify-content: space-around;width:50%;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="username-box">
+                    <div style="display:flex;">
+                            <h6 style="margin-bottom:0px; width:20%;">Username</h6>
+                            <div class="username-box-container">
+                                <h6 style="margin-bottom:0px;"><?php echo "$username"; ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="personal-info-box">
+                        <div style="display:flex;margin-bottom:3%">
+                            <h6 style="margin-bottom:0px; width:20%;">First name</h6>
+                            <div class="username-box-container">
+                                <h6 style="margin-bottom:0px;"><?php echo "$firstName"; ?></h6>
+                            </div>
+                        </div>
+                        <div style="display:flex;margin-bottom:3%">
+                            <h6 style="margin-bottom:0px; width:20%;">Last name</h6>
+                            <div class="username-box-container">
+                                <h6 style="margin-bottom:0px;"><?php echo "$lastName"; ?></h6>
+                            </div>
+                        </div>
+                        <div style="display:flex; margin-bottom:3%;">
+                            <div style="display:flex;width:50%">
+                                <h6 style="margin-bottom:0px; width:40%;">Gender</h6>
+                                <div class="username-box-container" style="width:40%">
+                                    <h6 style="margin-bottom:0px; margin-left:4%;"><?php echo "$gender"; ?></h6>
+                                </div>
+                            </div>
+                            <div style="display:flex;width:50%">
+                                <h6 style="margin-bottom:0px; width:40%;">Birth Date</h6>
+                                <div class="username-box-container" style="width:44%">
+                                    <h6 style="margin-bottom:0px;  margin-left:4%;"><?php echo "$bdate"; ?></h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="display:flex;">
+                            <h6 style="margin-bottom:0px; width:20%;">Email</h6>
+                            <div class="username-box-container">
+                                <h6 style="margin-bottom:0px;"><?php echo "$email"; ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="usertype-box">
+                        <div style="display:flex;">
+                            <h6 style="margin-bottom:0px; width:20%;">User Type</h6>
+                            <div class="username-box-container">
+                                <?php if ($usertyper == "teacher") { ?>
+                                    <h6 style="margin-bottom:0px;">Teacher</h6>
+                                <?php } ?>
+                                <?php if ($usertyper == "student") { ?>
+                                    <h6 style="margin-bottom:0px;">Student</h6>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="profile-main-box page" style="display:none;">
+                    <div class="name-box">
+                        <h5 class="open-sans-bold"><?php echo "$firstName";?> <?php echo "$lastName";?></h5>
+                    </div>
+                    <div class="profile-pic-box active">
+                        <h6 style="margin-bottom:0px;">Profile image</h6>
+                        <div style="display:flex;flex-direction:row;justify-content: space-around;margin-left:15%">
+                            <img src="user-profiles/1.png" alt="profile-pic">
+                            <div style="display:flex;flex-direction:row;justify-content: space-around;width:50%;">
+                                <button class="open-sans-regular">Upload</button>
+                                <button class="open-sans-regular" style="width:40%;color:#3a10e5;background-color:white;border:1px solid #3a10e5">Remove</button>
+                            </div>
+                        </div>
+                    </div>
+                    <form action="profile.php" method="POST" style="margin-bottom:0px;">
+                    <div class="username-box">
+                    <div style="display:flex;">
+                            <h6 style="margin-bottom:0px; width:20%;">Username</h6>
+                            <div class="username-box-container">
+                                <h6 style="margin-bottom:0px;"><?php echo "$username"; ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="personal-info-box">
+                        <div style="display:flex;margin-bottom:3%">
+                            <h6 style="margin-bottom:0px; width:20%;">First name</h6>
+                            <input type="text" name="firstName" value="<?php echo $firstName; ?>" class="username-box-container2" style="font-weight:600;margin-bottom:0px">
+                                </input>
+                        </div>
+                        <div style="display:flex;margin-bottom:3%">
+                            <h6 style="margin-bottom:0px; width:20%;">Last name</h6>
+                            <input type="text" name="lastName" value="<?php echo $lastName; ?>" class="username-box-container2" style="font-weight:600;margin-bottom:0px">
+                                </input>
+                        </div>
+                        <div style="display:flex; margin-bottom:3%;">
+                            <div style="display:flex;width:50%">
+                                <h6 style="margin-bottom:0px; width:40%;">Gender</h6>
+                                <select name="gender" class="username-box-container2" value="<?php echo $gender; ?>" style="font-weight:600;margin-bottom:0px;width:40%;">
+                                    <option selected value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </select>
+                            </div>
+                            <div style="display:flex;width:50%">
+                                <h6 style="margin-bottom:0px; width:40%;">Birth Date</h6>
+                                <input type="date" name="bdate" value="<?php echo $bdate; ?>" class="username-box-container2" style="font-weight:600;margin-bottom:0px;width:44%;">
+                                </input>
+                            </div>
+                        </div>
+                        <div style="display:flex;">
+                            <h6 style="margin-bottom:0px; width:20%;">Email</h6>
+                            <div class="username-box-container">
+                                <h6 style="margin-bottom:0px;"><?php echo "$email"; ?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="usertype-box2">
+                        <div style="display:flex;">
+                            <h6 style="margin-bottom:0px; width:20%;">User Type</h6>
+                            <div class="username-box-container2">
+                                <?php if ($usertyper == "teacher") { ?>
+                                    <h6 style="margin-bottom:0px;">Teacher</h6>
+                                <?php } ?>
+                                <?php if ($usertyper == "student") { ?>
+                                    <h6 style="margin-bottom:0px;">Student</h6>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="submit-box" style="width:100%;">
+                        <input class="submit-button-profile" type="submit" value="Save Changes" style="margin:auto; width:20%;"></input>
+                    <div>
+                </div>
+            </div> 
+        </div>
     </div>
-    </div>
+   </div>
+</div>
+<br>
+<br>
 </body>
 
 <script src="js/myhome-panel.js"></script>
